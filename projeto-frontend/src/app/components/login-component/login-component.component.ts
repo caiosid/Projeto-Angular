d4ecade5778248f6c460ef683383ecb816235d8b
caiocesar19 <111658365+caiocesar29@users.noreply.github.com>
2023-11-04 15:59:24 -0300

    Tela de Login
    Tela de Login pronta com Css e HTML ainda vou criar validações tanto no campo de email com de senha.

---
+++ projeto-frontend/src/app/components/login-component/login-component.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-component',
+  templateUrl: './login-component.component.html',
+  styleUrls: ['./login-component.component.css']
+})
+export class LoginComponentComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email, Validators.maxLength(50)]],
+      password: ['', [Validators.required, Validators.maxLength(15)]]
+    });
+  }
+
+}
