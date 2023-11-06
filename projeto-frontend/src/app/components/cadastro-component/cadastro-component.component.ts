e1bab3c98db4f44cb146b8560c9f60bc55ed9931
caiocesar19 <111658365+caiocesar29@users.noreply.github.com>
2023-11-06 07:43:10 -0300

    .
    .

--- projeto-frontend/src/app/components/cadastro-component/cadastro-component.component.ts
+++ projeto-frontend/src/app/components/cadastro-component/cadastro-component.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-component',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./cadastro-component.component.css']
 })
 export class CadastroComponentComponent {
+  formulario:FormGroup;
+
+  
+  constructor(private formBuilder: FormBuilder) {
+    this.formulario = this.formBuilder.group({
+      nome: [''],
+      email: [''],
+      cpf:['']
+    });
+  }
+
+  ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      nome: ['', [Validators.required]],
+      username: ['',  [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
 
 }
